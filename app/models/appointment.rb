af0d1e11ac4b0a3bf521918cd60bcbf9e297fa18
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-10-30 11:06:53 +0530

    third Commit

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,8 @@ require 'acts_as_appointment'
 
 class Appointment < ActiveRecord::Base
   # include ActsAsAppointment
+  # attr_accessible :from_time, :to_time
+  # acts_as_appointment
   acts_as_appointment(:from_time, :to_time)
   belongs_to :doctor
 end
